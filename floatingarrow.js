9abc79e398d81640a38fa8077f18fec4fe8da721
vpamrit <vishnuamritpydah@gmail.com>
2017-08-08 21:32:18 +0530

    adiofad

--- floatingarrow.js
+++ floatingarrow.js
@@ -40,7 +40,7 @@ $(function(){ /* to make sure the script runs after page load */
     $('body').on('touchmove', selScrollable, function(e) {
         // Only block default if internal div contents are large enough to scroll
         // Warning: scrollHeight support is not universal. (https://stackoverflow.com/a/15033226/40352)
-        if($(this)[0].scrollHeight > $(this).offsetWidth) {
+        if($(this)[0].scrollHeight > $(this).offsetHeight) {
             e.stopPropagation();
         }
     });
