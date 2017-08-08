57ba1bad43b2716628812bc91729edee86e91974
vpamrit <vishnuamritpydah@gmail.com>
2017-08-08 21:34:40 +0530

    adfjlkadf

--- floatingarrow.js
+++ floatingarrow.js
@@ -32,15 +32,16 @@ $(function(){ /* to make sure the script runs after page load */
 // added to, so bubbling would not work if we used document instead.
     $('body').on('touchstart', selScrollable, function(e) {
         if (e.currentTarget.scrollTop === 0) {
-            e.currentTarget.scrollTop = 1;
+            e.currentTarget.scrollTop = 5;
         } else if (e.currentTarget.scrollHeight === e.currentTarget.scrollTop + e.currentTarget.offsetHeight) {
-            e.currentTarget.scrollTop -= 1;
+            e.currentTarget.scrollTop -= 5;
         }
     });
+
     $('body').on('touchmove', selScrollable, function(e) {
         // Only block default if internal div contents are large enough to scroll
         // Warning: scrollHeight support is not universal. (https://stackoverflow.com/a/15033226/40352)
-        if($(this)[0].scrollHeight > $(this).offsetHeight) {
+        if($(this)[0].scrollHeight > $(this).innerHeight()) {
             e.stopPropagation();
         }
     });
