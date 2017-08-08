7c6443126ff2b216b06eb2d009821cd4ff95075d
vpamrit <vishnuamritpydah@gmail.com>
2017-08-08 16:14:13 +0530

    hi

--- floatingarrow.js
+++ floatingarrow.js
@@ -22,27 +22,27 @@ $(function(){ /* to make sure the script runs after page load */
     //     e.stopPropagation();
     // });
     //
+
+    var selScrollable = '.box';
+// Uses document because document will be topmost level in bubbling
     $(document).on('touchmove',function(e){
         e.preventDefault();
     });
-
-    $('body').on('touchstart','.box',function(e) {
-
-        // Only execute the below code once at a time
-        if (!isScrolling) {
-            isScrolling = true;
-            if (e.currentTarget.scrollTop === 0) {
-                e.currentTarget.scrollTop = 1;
-            } else if (e.currentTarget.scrollHeight === e.currentTarget.scrollTop + e.currentTarget.offsetHeight) {
-                e.currentTarget.scrollTop -= 1;
-            }
-            isScrolling = false;
+// Uses body because jQuery on events are called off of the element they are
+// added to, so bubbling would not work if we used document instead.
+    $('body').on('touchstart', selScrollable, function(e) {
+        if (e.currentTarget.scrollTop === 0) {
+            e.currentTarget.scrollTop = 1;
+        } else if (e.currentTarget.scrollHeight === e.currentTarget.scrollTop + e.currentTarget.offsetHeight) {
+            e.currentTarget.scrollTop -= 1;
         }
     });
-
-    // Prevents preventDefault from being called on document if it sees a scrollable div
-    $('body').on('touchmove','.box',function(e) {
-        e.stopPropagation();
+    $('body').on('touchmove', selScrollable, function(e) {
+        // Only block default if internal div contents are large enough to scroll
+        // Warning: scrollHeight support is not universal. (https://stackoverflow.com/a/15033226/40352)
+        if($(this)[0].scrollHeight > $(this).innerHeight()) {
+            e.stopPropagation();
+        }
     });
 
     $("#body").removeClass("preload");
