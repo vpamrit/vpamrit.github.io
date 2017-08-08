29feea755c63af7462696b05f5f9c121edd3f719
vpamrit <vishnuamritpydah@gmail.com>
2017-08-08 19:36:23 +0530

    bugfixing safari mobile

--- floatingarrow.js
+++ floatingarrow.js
@@ -23,27 +23,27 @@ $(function(){ /* to make sure the script runs after page load */
     // });
     //
 
-    var selScrollable = '.box';
-// Uses document because document will be topmost level in bubbling
-    $(document).on('touchmove',function(e){
-        e.preventDefault();
-    });
-// Uses body because jQuery on events are called off of the element they are
-// added to, so bubbling would not work if we used document instead.
-    $('body').on('touchstart', selScrollable, function(e) {
-        if (e.currentTarget.scrollTop === 0) {
-            e.currentTarget.scrollTop = 1;
-        } else if (e.currentTarget.scrollHeight === e.currentTarget.scrollTop + e.currentTarget.offsetHeight) {
-            e.currentTarget.scrollTop -= 1;
-        }
-    });
-    $('body').on('touchmove', selScrollable, function(e) {
-        // Only block default if internal div contents are large enough to scroll
-        // Warning: scrollHeight support is not universal. (https://stackoverflow.com/a/15033226/40352)
-        if($(this)[0].scrollHeight > $(this).innerHeight()) {
-            e.stopPropagation();
-        }
-    });
+//     var selScrollable = '.box';
+// // Uses document because document will be topmost level in bubbling
+//     $(document).on('touchmove',function(e){
+//         e.preventDefault();
+//     });
+// // Uses body because jQuery on events are called off of the element they are
+// // added to, so bubbling would not work if we used document instead.
+//     $('body').on('touchstart', selScrollable, function(e) {
+//         if (e.currentTarget.scrollTop === 0) {
+//             e.currentTarget.scrollTop = 1;
+//         } else if (e.currentTarget.scrollHeight === e.currentTarget.scrollTop + e.currentTarget.offsetHeight) {
+//             e.currentTarget.scrollTop -= 1;
+//         }
+//     });
+//     $('body').on('touchmove', selScrollable, function(e) {
+//         // Only block default if internal div contents are large enough to scroll
+//         // Warning: scrollHeight support is not universal. (https://stackoverflow.com/a/15033226/40352)
+//         if($(this)[0].scrollHeight > $(this).innerHeight()) {
+//             e.stopPropagation();
+//         }
+//     });
 
     $("#body").removeClass("preload");
 
