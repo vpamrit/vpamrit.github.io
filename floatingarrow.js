57863ac4d7428732f749e99d901e30509116a455
vpamrit <vishnuamritpydah@gmail.com>
2017-08-08 14:26:52 +0530

    bugfixing

--- floatingarrow.js
+++ floatingarrow.js
@@ -7,20 +7,20 @@ $(document).ready(function(){
 
 $(function(){ /* to make sure the script runs after page load */
     $(window).scrollTop(0);
-    // $(document).bind(
-    //     'touchmove',
-    //     function(e) {
-    //         e.preventDefault();
-    //     }
-    // );
-    //
-    // $('.box').each(function() {
-    //     $(this).addClass('scrollable');
-    // });
-    //
-    // $('body').on('touchmove','.scrollable',function(e) {
-    //     e.stopPropagation();
-    // });
+    $(document).bind(
+        'touchmove',
+        function(e) {
+            e.preventDefault();
+        }
+    );
+
+    $('.box').each(function() {
+        $(this).addClass('scrollable');
+    });
+
+    $('body').on('touchmove','.scrollable',function(e) {
+        e.stopPropagation();
+    });
 
     $("#body").removeClass("preload");
 
